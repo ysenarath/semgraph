cac0d7ee8577317778a3f5fa365efe9c920f1e56
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2026-05-03 11:01:57 -0400

    Refactor variable names for clarity in get_entries_from_string function

--- src/semgraph/evaluate.py
+++ src/semgraph/evaluate.py
@@ -4,8 +4,8 @@ import re
 def get_entries_from_string(data: str) -> list[str]:
     entries = []
     for e in data.split("\n\n"):
-        lines = [l.strip() for l in e.splitlines()]
-        lines = [l for l in lines if (l and not l.startswith("#"))]
+        lines = [line.strip() for line in e.splitlines()]
+        lines = [line for line in lines if (line and not line.startswith("#"))]
         string = " ".join(lines)
         string = string.replace("\t", " ")  # replace tabs with a space
         string = re.sub(" +", " ", string)  # squeeze multiple spaces into a single
